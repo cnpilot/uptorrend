2db750af3efe0f7342feda35ca32823953d99239
cnpilot <32807915+cnpilot@users.noreply.github.com>
2024-05-03 12:04:06 +0800

    Update rcp.sh

--- rcp.sh
+++ rcp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#/home/boxbox/torcp/rcp.sh "%F" "%N" "%G" "%D" "%L" "%R" "%C" "%Z" "%I"
+#/home/boxbox/torcp/rcp.sh "%I"
 # 从命令行获取传递的参数
-i="$9"
+i="$1"
 
 # 执行 Python 脚本并传递参数
 python3 /home/boxbox/set_autorun_preferences.py "$i"
